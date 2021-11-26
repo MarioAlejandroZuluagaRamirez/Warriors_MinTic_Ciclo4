aba0b1e4d16b7081ee29fcbe7940ee8bca21be3e
MarioAlejandroZuluagaRamirez <mazuluagar@gmail.com>
2021-11-26 16:06:22 -0500

    Recuperacion Frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { ContentComponent } from './template/content/content.component';
+import { ErrorComponent } from './template/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContentComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
